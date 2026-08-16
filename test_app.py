03675feaaee3624c8f7a40d45fcbd7ed454e03c1
Injamrajesh <injamrajesh85@gmail.com>
2026-08-16 07:58:53 +0530

    updated test_app for failure test

--- test_app.py
+++ test_app.py
@@ -23,7 +23,7 @@ def test_health(client):
     """Health endpoint should return HTTP 200."""
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json["status"] == "healthy"
+    assert response.json["status"] == "FAILED_TEST"
 
 
 def test_home_page(client):
